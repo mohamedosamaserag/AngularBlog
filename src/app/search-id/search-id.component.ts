b1a1087eee74a53eb5959ab9ef5463277248910c
mahmoudkhaled7422 <77900131+mahmoudkhaled7422@users.noreply.github.com>
2021-02-05 20:52:24 +0200

    one

---
+++ src/app/search-id/search-id.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-id',
+  templateUrl: './search-id.component.html',
+  styleUrls: ['./search-id.component.css']
+})
+export class SearchIdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/search-title/search-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-title',
+  templateUrl: './search-title.component.html',
+  styleUrls: ['./search-title.component.css']
+})
+export class SearchTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
